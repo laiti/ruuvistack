f4591a102d633e6e19b00d4456daa4514edf8766
Antti Laiti <antti@laiti.fi>
2025-02-16 16:22:18 +0200

    Fix architecture image link and path to Telegram alert template

--- tools/debian-install-packages.sh
+++ tools/debian-install-packages.sh
@@ -2,8 +2,4 @@
 
 # All required packages in Debian 12.9:
 
-<<<<<<< HEAD
 apt-get install docker.io make git docker-compose apparmor influxdb-client
-=======
-apt-get install docker.io make git docker-compose apparmor influxdb-client
->>>>>>> 9136f2d (Remove git checkout command and container update moved to Makefile)
