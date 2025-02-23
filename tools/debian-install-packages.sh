9136f2d8487b045563bef7226c34c756dd228db7
Antti Laiti <antti@laiti.fi>
2025-02-23 12:42:23 +0200

    Remove git checkout command and container update moved to Makefile

--- tools/debian-install-packages.sh
+++ tools/debian-install-packages.sh
@@ -3,5 +3,3 @@
 # All required packages in Debian 12.9:
 
 apt-get install docker.io make git docker-compose apparmor influxdb-client
-
-git clone git@github.com:laiti/ruuvistack.git /var/lib/ruuvistack
