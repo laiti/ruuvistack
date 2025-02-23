0b23750c76e1fc37d8dfe0e24658ba37835ca6e2
Antti Laiti <antti@laiti.fi>
2025-02-23 12:42:23 +0200

    Remove git checkout command and container update moved to Makefile

--- tools/debian-install-packages.sh
+++ tools/debian-install-packages.sh
@@ -2,4 +2,8 @@
 
 # All required packages in Debian 12.9:
 
+<<<<<<< HEAD
 apt-get install docker.io make git docker-compose apparmor influxdb-client
+=======
+apt-get install docker.io make git docker-compose apparmor influxdb-client
+>>>>>>> 9136f2d (Remove git checkout command and container update moved to Makefile)
